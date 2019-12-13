6bc9654318654b6b728837212bafa8ea3c779243
Daniel Dallagnese <dallagnese@gmail.com>
2019-12-13 15:06:51 -0300

    Aula 96
    App Frases do Dia finalizado

--- app02_Frases/App.js
+++ app02_Frases/App.js
@@ -1,114 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { Text, View, Image, TouchableOpacity, Alert } from 'react-native';
+import { white } from 'ansi-colors';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Estilos = {
+    principal: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    botao: {
+        backgroundColor: '#538530',
+        paddingVertical: 10,
+        paddingHorizontal: 40,
+        marginTop: 20
+    },
+    textoBotao: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold'
+    }
+}
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+const gerarNovaFrase = () => {
+    var numeroAleatorio = Math.floor(Math.random() * 5)
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+    var frases = Array()
+    frases[0] = 'Frase 1'
+    frases[1] = 'Frase 2'
+    frases[2] = 'Frase 3'
+    frases[3] = 'Frase 4'
+    frases[4] = 'Frase 5'
+    Alert.alert(frases[numeroAleatorio])
+}
+
+const App = () => {
+    return (
+        <View style={Estilos.principal}>
+            <Image source={require('./imgs/logo.png')} />
+            <TouchableOpacity
+                onPress={gerarNovaFrase}
+                style={Estilos.botao}>
+                <Text style={Estilos.textoBotao}>
+                    Nova Frase
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
 
 export default App;
