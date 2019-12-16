8baeb514c911848b37daad884fb505def2ef19ae
Daniel Dallagnese <dallagnese@gmail.com>
2019-12-16 13:59:28 -0300

    Aula 121

--- app04_CatalogoProdutos/src/components/ListaItens.js
+++ app04_CatalogoProdutos/src/components/ListaItens.js
@@ -4,15 +4,25 @@ import {
   View,
   Text,
 } from 'react-native';
+import axios from 'axios'
 import Itens from './Itens';
 
 export default class ListaItens extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { listaItens: [] }
+    }
+
+    componentWillMount() {
+        axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+        .then(response => { this.setState({ listaItens: response.data }) })
+        .catch(err => { console.log('Erro ao recuperar os dados '+err) })
+    }
+
     render() {
         return (
             <View>
-                <Itens />
-                <Itens />
-                <Itens />
+                { console.log(this.state.listaItens) }
             </View>
         )
     }
